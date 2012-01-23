1d238410633a2a4b8bd962f625d87769d92bf0fd
MTLZ <thomas.noury@gmail.com>
2012-01-23 10:01:08 +0100

    Added forgotten files in previous commit

---
+++ include/3d/MeshAnimationInfo.h
@@ -0,0 +1,19 @@
+/******************************************************************************
+Invisible Spirit by Thomas Noury is licensed under a Creative Commons
+Attribution-NonCommercial-ShareAlike 3.0 Unported License. Based on a work at
+is06.com. Permissions beyond the scope of this license may be available at
+http://www.is06.com. Legal code in license.txt
+*******************************************************************************/
+
+#ifndef __IS06_MESH_ANIMATION_INFO_H__
+#define __IS06_MESH_ANIMATION_INFO_H__
+
+using namespace irr;
+
+struct MeshAnimationInfo {
+  u32 startFrame;
+  u32 endFrame;
+  bool looped;
+};
+
+#endif
