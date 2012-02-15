22d60bbf14ce4f5c179293ceb28f269a8fa17ff9
Thomas Noury <thomas.noury@is06.com>
2012-02-16 00:49:43 +0100

    CMake scripts updated and work on Windows, thanks Christopho! (https://github.com/christopho)

--- CMakeModules/FindFmodEx.cmake
+++ CMakeModules/FindFmodEx.cmake
@@ -1,57 +1,27 @@
-IF(FMODEX_LIBRARIES AND FMODEX_INCLUDE_DIRS)
-  set(FMODEX_FOUND TRUE)
-ELSE(FMODEX_LIBRARIES AND FMODEX_INCLUDE_DIRS)
-  find_path(
-    FMODEX_INCLUDE_DIR
-    NAMES
-      fmod.h
-    PATHS
-      ${CMAKE_INCLUDE_PATH}
-      ${CMAKE_INCLUDE_PATH}/fmod
-      ${CMAKE_INCLUDE_PATH}/fmodex
-      /usr/include
-      /usr/include/fmod
-      /usr/include/fmodex
-      /usr/local/include
-      /usr/local/include/fmod
-      /usr/local/include/fmodex
-      /opt/local/include
-      /sw/include
-  )
-  find_library(
-    FMODEX_LIBRARY
-    NAMES
-      FmodEx
-      fmod
-    PATHS
-      ${CMAKE_LIBRARY_PATH}
-      ${CMAKE_LIBRARY_PATH}/fmod
-      ${CMAKE_LIBRARY_PATH}/fmodex
-      /usr/lib
-      /usr/lib/fmod
-      /usr/lib/fmodex
-      /usr/local/lib
-      /usr/local/lib/fmod
-      /usr/local/lib/fmodex
-      /opt/local/lib
-      /sw/lib
-  )
+find_path(
+  FMODEX_INCLUDE_DIR
+	NAMES
+		fmod.h
+  PATH_SUFFIXES
+		fmod
+		fmodex
+)
 
-  IF(FMODEX_LIBRARY)
-    set(FMODEX_FOUND TRUE)
-  ENDIF(FMODEX_LIBRARY)
+find_library(
+	FMODEX_LIBRARY
+  NAMES
+		fmodex
+	PATH_SUFFIXES
+		fmod
+		fmodex
+)
 
-  set(FMODEX_INCLUDE_DIRS ${FMODEX_INCLUDE_DIR})
+include(FindPackageHandleStandardArgs)
 
-  IF(FMODEX_FOUND)
-    set(FMODEX_LIBRARIES ${FMODEX_LIBRARIES} ${FMODEX_LIBRARY})
-  ELSE(FMODEX_FOUND)
-    message(FATAL_ERROR "FmodEx not found!")
-  ENDIF(FMODEX_FOUND)
+find_package_handle_standard_args(
+	FMODEX DEFAULT_MSG
+	FMODEX_INCLUDE_DIR
+	FMODEX_LIBRARY
+)
 
-  IF(FMODEX_INCLUDE_DIRS AND FMODEX_LIBRARIES)
-    set(FMODEX_FOUND TRUE)
-  ENDIF(FMODEX_INCLUDE_DIRS AND FMODEX_LIBRARIES)
-
-  mark_as_advanced(FMODEX_INCLUDE_DIRS FMODEX_LIBRARIES)
-ENDIF(FMODEX_LIBRARIES AND FMODEX_INCLUDE_DIRS) 
+mark_as_advanced(FMODEX_INCLUDE_DIR FMODEX_LIBRARY)
--- CMakeModules/FindIrrlicht.cmake
+++ CMakeModules/FindIrrlicht.cmake
@@ -1,53 +1,27 @@
-IF(IRRLICHT_LIBRARIES AND IRRLICHT_INCLUDE_DIRS)
-  set(IRRLICHT_FOUND TRUE)
-ELSE(IRRLICHT_LIBRARIES AND IRRLICHT_INCLUDE_DIRS)
-  find_path(
-    IRRLICHT_INCLUDE_DIR
-    NAMES
-      irrlicht.h
-    PATHS
-      ${CMAKE_INCLUDE_PATH}
-      ${CMAKE_INCLUDE_PATH}/irr
-      ${CMAKE_INCLUDE_PATH}/irrlicht
-      /usr/include
-      /usr/include/irr
-      /usr/include/irrlicht
-      /usr/local/include
-      /usr/local/include/irr
-      /usr/local/include/irrlicht
-      /opt/local/include
-      /sw/include
-  )
-  find_library(
-    IRRLICHT_LIBRARY
-    NAMES
-      Irrlicht
-    PATHS
-      ${CMAKE_LIBRARY_PATH}
-      ${CMAKE_LIBRARY_PATH}/irr
-      ${CMAKE_LIBRARY_PATH}/irrlicht
-      /usr/lib
-      /usr/lib/irr
-      /usr/lib/irrlicht
-      /usr/local/lib
-      /opt/local/lib
-      /sw/lib
-  )
-  IF(IRRLICHT_LIBRARY)
-    set(IRRLICHT_FOUND TRUE)
-  ENDIF(IRRLICHT_LIBRARY)
+find_path(
+  IRRLICHT_INCLUDE_DIR
+	NAMES
+		irrlicht.h
+  PATH_SUFFIXES
+		irr
+		irrlicht
+)
 
-  set(IRRLICHT_INCLUDE_DIRS ${IRRLICHT_INCLUDE_DIR})
+find_library(
+	IRRLICHT_LIBRARY
+  NAMES
+		Irrlicht
+	PATH_SUFFIXES
+		irr
+		irrlicht
+)
 
-  IF(IRRLICHT_FOUND)
-    set(IRRLICHT_LIBRARIES ${IRRLICHT_LIBRARIES} ${IRRLICHT_LIBRARY})
-  ELSE(IRRLICHT_FOUND)
-    message(FATAL_ERROR "Irrlicht not found!")
-  ENDIF(IRRLICHT_FOUND)
+include(FindPackageHandleStandardArgs)
 
-  IF(IRRLICHT_INCLUDE_DIRS AND IRRLICHT_LIBRARIES)
-    set(IRRLICHT_FOUND TRUE)
-  ENDIF(IRRLICHT_INCLUDE_DIRS AND IRRLICHT_LIBRARIES)
+find_package_handle_standard_args(
+	IRRLICHT DEFAULT_MSG
+	IRRLICHT_INCLUDE_DIR
+	IRRLICHT_LIBRARY
+)
 
-  mark_as_advanced(IRRLICHT_INCLUDE_DIRS IRRLICHT_LIBRARIES)
-ENDIF(IRRLICHT_LIBRARIES AND IRRLICHT_INCLUDE_DIRS) 
+mark_as_advanced(IRRLICHT_INCLUDE_DIR IRRLICHT_LIBRARY)
