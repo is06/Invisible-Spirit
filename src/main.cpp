226a97d18b3ef73e7c39023c6d75a94d8a3a7f2d
is06 <thomas.noury@is06.com>
2012-02-21 12:41:50 +0100

    Windows dialog boxes integration

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,12 @@ http://www.is06.com. Legal code in license.txt
 #include "../include/Game.h"
 #include "../include/EngineException.h"
 
+#ifdef _IRR_WINDOWS_
+#include <windows.h>
+INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT)
+#else
 int main()
+#endif
 {
   try {
     Game::init();
@@ -17,6 +22,9 @@ int main()
     Game::finish();
   } catch(const std::exception& e) {
     cerr << "Exception: " << e.what() << endl;
+#ifdef _IRR_WINDOWS_
+    MessageBoxA(NULL, e.what(), "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+#endif
     Game::errorLog(e);
   }
   return 0;
